edfadc4a119579e071bb68de69886932c54bc538
Mu Yang <emfomy@gmail.com>
2021-01-26 14:46:22 +0800

    Update version number to 3.0.0.

--- ehn/__init__.py
+++ ehn/__init__.py
@@ -6,7 +6,7 @@ __author_email__ = 'emfomy@gmail.com'
 __copyright__ = '2018-2020 CKIP Lab'
 
 __title__ = 'E-HowNet'
-__version__ = '1.0.0'
+__version__ = '3.0.0'
 __description__ = 'E-HowNet Utilities'
 __license__ = 'GPL-3.0'
 
